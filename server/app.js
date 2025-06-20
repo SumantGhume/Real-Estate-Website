75e00781b39ec53fdf04f506ead43b51ebce33a8
SumantGhume <116431128+SumantGhume@users.noreply.github.com>
2025-06-20 21:13:58 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173", 
+  origin: "https://realestatesumant.netlify.app/", 
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true 
 }));
